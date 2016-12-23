ad36330923e05763a7607f623b98c383bed727e2
Fredrik Lidström <fredli74@gmail.com>
2016-12-23 11:36:22 +0100

    ProcessRunning terminated the other process when it shouldn’t on unix systems

--- lockfile_unix.go
+++ lockfile_unix.go
@@ -8,11 +8,12 @@ package lockfile
 
 import (
 	"os"
+	"syscall"
 )
 
 // ProcessRunning is a cross-platform check to work on both Windows and Unix systems as the os.FindProcess() function works differently.
 func ProcessRunning(pid int) bool {
 	p, _ := os.FindProcess(pid)   // On unix the FindProcess never returns an error
-	err := p.Signal(os.Interrupt) // Returns error if process is not running
+	err := p.Signal(syscall.Signal(0)) // Returns error if process is not running
 	return err == nil
 }
