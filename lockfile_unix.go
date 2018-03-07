4cf6cdd251d713031367e3401008b597a8ad595c
Mike Mozhaev <programmer10110@yandex.ru>
2018-03-07 15:35:13 +0300

    Update lockfile_unix.go

--- lockfile_unix.go
+++ lockfile_unix.go
@@ -13,7 +13,10 @@ import (
 
 // ProcessRunning is a cross-platform check to work on both Windows and Unix systems as the os.FindProcess() function works differently.
 func ProcessRunning(pid int) bool {
-	p, _ := os.FindProcess(pid)   // On unix the FindProcess never returns an error
-	err := p.Signal(syscall.Signal(0)) // Returns error if process is not running
+	p, _ := os.FindProcess(pid)                                 // On unix the FindProcess never returns an error
+	err := p.Signal(syscall.Signal(0))                          // Returns error if process is not running
+	if err != nil && err.Error() == "operation not permitted" { // err equals "operation not permitted", when process is running from another user
+		return true
+	}
 	return err == nil
 }
